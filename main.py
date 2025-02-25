c093bfcac8e379e783065d98f0f84b07fadabbdf
Edoardo Vergani <edoardo.vergani@soft.it>
2025-02-25 17:38:08 +0100

    Test deployment update

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ import functions_framework
 @functions_framework.http
 def hello_http(request):
     """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
     request_json = request.get_json(silent=True)
     request_args = request.args
@@ -19,5 +12,7 @@ def hello_http(request):
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
-        name = 'World'
-    return 'Hello {}!'.format(name)
+        name = 'Cloud Run'
+
+    return 'Hello {}! Deployment Test Successful!'.format(name)
+
